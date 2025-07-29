1000bc8682feb64d505e6aebc0b88590362b08de
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-07-29 14:15:49 +0800

    adding register ui

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,7 @@ const Input = ({placeholder, type}) => {
   return (
     <>
         <div className='flex flex-col gap-1'>
-          <input type={type} placeholder={placeholder} className=' text-sm p-3 rounded-xl text-gray-700 outline-none border  border-gray-300' />
+          <input type={type} placeholder={placeholder} className='w-full text-sm p-3 rounded-xl text-gray-700 outline-none border  border-gray-300' />
         </div>
     </>
   )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,16 +4,23 @@ import Button from "../components/Button";
 
 const Register = () => {
   return (
-    <div className='flex flex-col justify-between w-screen h-screen p-6'>
+    <div className='flex flex-col justify-between w-screen h-screen p-4'>
       <h1 className='text-center'>Englis US</h1>
 
       <div className='flex justify-center'>
         <i className="fa-brands fa-facebook-messenger text-blue-600 text-6xl"></i>
       </div>
 
+
+
       <form className='flex flex-col gap-4'>
+        <div className='flex gap-2 w-full'>
+            <Input type='text' placeholder='First name' />
+            <Input type='text' placeholder='Last name'  />
+        </div>
         <Input type='email' placeholder='Mobile number or email' />
         <Input type='password' placeholder='Password' />
+        <Input type='password' placeholder='Confirm Password' />
         <div>
           <Button text='Register' bgColor='blue' type='submit' />
         </div>
